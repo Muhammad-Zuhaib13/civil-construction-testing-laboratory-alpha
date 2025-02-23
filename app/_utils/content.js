162f71be3110a05fd3ebbe9309fc265d67856391
Muhammad-Zuhaib13 <muhammadzohaib578@gmail.com>
2025-02-23 18:28:18 +0500

    udated structure

---
+++ app/_utils/content.js
@@ -0,0 +1,131 @@
+const home = {
+  seo: {},
+  data: [
+    {
+      key: "banner",
+      pageContent: {
+        title: "Civil Construction Testing Laboratory",
+        img: "/assets/images/home.jpg",
+        para: "",
+      },
+    },
+    {
+      key: "textWithImage",
+      pageContent: {
+        subTItleTop: "Civil Engineering Projects",
+        title: "CCT LABS USES ADVANCED TECHNIQUES TO PROVIDE ACCURATE RESULTS.",
+        paras: [
+          "Civil Engineering is both a science and an art. And the quality of results depends both upon the skill and training of the Engineer, but also on the quality of information and data available on each type of engineering project. That’s where CCT comes in. We have a deep understanding of Civil Engineering Lab Tests and the types of testing that must be conducted to define the quality and strength of the material involved in the construction process for our civil engineering clients.",
+          "At CCT, we maintain a staff of experienced personnel and calibrated equipment to provide the most accurate results that our civil engineering clients trust and depend on. Our Testing experts have decades of experience and are certified in a number of testing methods and have certifications from many regulatory agencies. Including the American Society of Testing and Materials (ASTM). By using state-of-the-art equipment and advanced techniques, we provide exacting solutions for a wide range of civil engineering applications.",
+        ],
+        img: {
+          src: "/assets/images/home-comp-txt-img.jpg",
+          alt: "Scenic view of Oman",
+        },
+      },
+    },
+  ],
+};
+const about = {
+  seo: {},
+  data: [
+    {
+      key: "banner",
+      pageContent: {
+        title: "About Civil Construction Testing Laboratory",
+        img: "/assets/images/about.jpg",
+        para: "",
+      },
+    },
+    {
+      key: "textWithImage",
+      pageContent: {
+        subTItleTop: "",
+        title: "What We're All About.",
+        paras: [
+          "CCT is known for high quality, civil engineering and construction inspection services, and testing. CCT was founded by Mitch Forst, P.E. in 1994. Mitch has dedicated over 30 years of his career to inspecting, managing and surveying construction projects all across the Midwest. As a result of this close affiliation with the Construction Industry, CCT is exceptionally well qualified to provide complete construction materials, mixes and soils testing services.",
+        ],
+        img: {
+          src: "/assets/images/home-comp-txt-img.jpg",
+          alt: "About",
+        },
+      },
+    },
+    {
+      key: "heading",
+      pageContent: {
+        title:
+          "We are the Michigan Engineering, Construction and Testing Experts.",
+      },
+    },
+  ],
+};
+const careers = {
+  seo: {},
+  data: [
+    {
+      key: "banner",
+      pageContent: {
+        title: "Careers",
+        img: "/assets/images/careers.jpg",
+        para: "",
+      },
+    },
+    {
+      key: "textWithImage",
+      pageContent: {
+        subTItleTop: "",
+        title: "Careers",
+        paras: [
+          "CCT is growing rapidly in Michigan. And we are looking for talented people who want to grow your expertise and your career with the strength of experience working directly on Engineering and Construction Testing every day. At CCT, you will work directly with experts and professional leaders who can help your career grow significantly from day one. And beyond.",
+        ],
+        img: {
+          src: "/assets/images/home-comp-txt-img.jpg",
+          alt: "Scenic view of Oman",
+        },
+      },
+    },
+  ],
+};
+const contactUs = {
+  seo: {},
+  data: [
+    {
+      key: "banner",
+      pageContent: {
+        title: "Contact Us",
+        img: "/assets/images/contact.jpg",
+        para: "",
+      },
+    },
+    {
+      key: "textWithImage",
+      pageContent: {
+        subTItleTop: "",
+        title: "Put us to the Test, Michigan!",
+        paras: [
+          "Call CCT for a project quote today at 419-878-7304. Or simply submit your request on the form below. We’ll respond promptly.",
+          "CCT will help you obtain high-quality data, test reports, and certificates that you can rely on to make informed decisions about construction materials and stay within compliance. Engage with one of our experts today.",
+        ],
+        img: {
+          src: "/assets/images/home-comp-txt-img.jpg",
+          alt: "Scenic view of Oman",
+        },
+      },
+    },
+    {
+      key: "heading",
+      pageContent: {
+        title:
+          "We are the Michigan Engineering, Construction and Testing Experts.",
+      },
+    },
+    {
+        key:'contactForm',
+        pageContent:{
+            
+        }
+    }
+  ],
+};
+export { home, about, careers, contactUs };
